7be5333f772755d2bd3239b3941d7d37fe18186f
DESKTOP-3GUQ93R\GA2B <h1202315001@hiratagakuen.onmicrosoft.com>
2024-05-13 16:35:48 +0900

    ロッカーに入るときのプレイヤーの制限や視界の制限

---
+++ Colubia/Assets/ore/fadeCTRL.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fadeCTRL : MonoBehaviour
+{
+    SpriteRenderer sp;
+    Color spriteColor;
+    float duration = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sp = GetComponent<SpriteRenderer>();
+        spriteColor = sp.color;
+        StartCoroutine(Fade(1));
+    }
+
+    IEnumerator Fade(float targetAlpha)
+    {
+        while (!Mathf.Approximately(spriteColor.a, targetAlpha))
+        {
+            float changePerFrame = Time.deltaTime / duration;
+            spriteColor.a = Mathf.MoveTowards(spriteColor.a, targetAlpha, changePerFrame);
+            sp.color = spriteColor;
+            yield return null;
+        }
+    }
+
+}
